1d66c8b9aa21bce091717fdb25531e7837d2949c
mohsenrs <mohsen8022@gmail.com>
2021-04-12 21:41:33 +0430

    2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
-const cityForm = document.querySelector('.change-location')
-cityForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const city = cityForm.city.value.trim()
-    cityForm.reset()
-})
+const cityForm = document.querySelector(".change-location");
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {
+    cityDets: cityDets,
+    weather: weather,
+  };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update ui
+  updateCity(city)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
--- js/forecast.js
+++ js/forecast.js
@@ -1,25 +1,25 @@
-const key = '2GhKN9YdWEBqoyAuVJ2EhaYILh2IvMGF'
+const key = 'GfEDZweDjoD7zSmDLdJTI27SFpEszKHk'
 
-// get information
+// get weather information
 const getWeather = async (id) => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
     const query = `${id}?apikey=${key}`
+
     const response = await fetch(base + query)
     const data = await response.json()
+
     return data[0]
 }
 
-// get city
+// get city information
 const getCity = async (city) => {
+
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${key}&q=${city}`
+
     const response = await fetch(base + query)
     const data = await response.json()
-    // return data
+
     return data[0]
 }
-getCity('manchester')
-    .then(data => {
-        return getWeather(data.Key)
-    }).then(data => console.log(data))
-    .catch(err => console.log('ridi', err.message))
+
